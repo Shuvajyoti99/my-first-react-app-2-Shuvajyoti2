e9a17da6cf61320ad27fff92f6b2b09bb633cc69
Shuvajyoti99 <63437730+Shuvajyoti99@users.noreply.github.com>
2020-04-19 09:31:29 +0530

    Add files via upload

---
+++ build/precache-manifest.27fc63409478bb17e9dd9d433496891a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c4d245ec8d15a0fcbb73acf1f88940",
+    "url": "/my-first-react-app-2-Shuvajyoti2/index.html"
+  },
+  {
+    "revision": "0800a8f736d46cf17e73",
+    "url": "/my-first-react-app-2-Shuvajyoti2/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "f8f1906bf5efe9bf0309",
+    "url": "/my-first-react-app-2-Shuvajyoti2/static/css/main.0d16e165.chunk.css"
+  },
+  {
+    "revision": "0800a8f736d46cf17e73",
+    "url": "/my-first-react-app-2-Shuvajyoti2/static/js/2.df3de0a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react-app-2-Shuvajyoti2/static/js/2.df3de0a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8f1906bf5efe9bf0309",
+    "url": "/my-first-react-app-2-Shuvajyoti2/static/js/main.da696660.chunk.js"
+  },
+  {
+    "revision": "ca1c90d70007005edb77",
+    "url": "/my-first-react-app-2-Shuvajyoti2/static/js/runtime-main.5b9fa16a.js"
+  }
+]);
